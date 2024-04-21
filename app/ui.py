60585284e2f857c96921adbdfc04a7c361f71e07
Mitchel Hsu <40386247+Mitchelhsu@users.noreply.github.com>
2024-04-21 11:30:40 -0700

    feat: implement ui error handling

--- app/ui.py
+++ app/ui.py
@@ -2,36 +2,40 @@ import time
 import requests
 import gradio as gr
 from utils import get_url_list
-from models import SubmitQuestionAndDocumentRequest, GetQuestionAndFactsResponse
+from models import SubmitQuestionAndDocumentRequest, GetQuestionAndFactsResponse, SubmitQuestionAndDocumentsResponse
 
-base_url = 'https://cleric-agent-ad76f992e8d8.herokuapp.com/'
+base_url = 'https://cleric-agent-api-untxx3isja-uc.a.run.app'
+# base_url = 'http://localhost:8000'
 
 
 def fetch_facts(question, call_log_urls):
+    if len(call_log_urls) == 0:
+        raise gr.Error('Please input call log.')
+
+    if len(question) == 0:
+        raise gr.Error('Please input question.')
+
     urls = get_url_list(call_log_urls)
     payload = SubmitQuestionAndDocumentRequest(
         question=question,
-        urls=urls
+        documents=urls
     ).dict()
+
     response = requests.post(f"{base_url}/submit_question_and_documents", json=payload)
+    response = SubmitQuestionAndDocumentsResponse(**response.json())
+    if response.status != 'success':
+        raise gr.Error('Input error: ' + response.status)
 
     start_time = time.time()
     while True:
         response = requests.get(f"{base_url}/get_question_and_facts")
         if response.status_code != 200:
-            # st.error(f"Unexpected status code when getting question and facts: {response.status_code}")
-            return None
-        try:
-            data = GetQuestionAndFactsResponse(**response.json())
-        except ValueError as e:
-            # st.error(f"The response data does not match the expected schema: {str(e)}")
-            # st.write(response.json())  # Print the invalid data for debugging
-            return None
+            raise gr.Error('Server response error.')
 
+        data = GetQuestionAndFactsResponse(**response.json())
         if data.status == "done":
             break
-        elif time.time() - start_time > 300:  # 5 minutes timeout
-            # st.error("Timeout: Facts not ready after 5 minutes")
+        elif time.time() - start_time > 300:
             return None
         time.sleep(1)
 
@@ -42,29 +46,24 @@ with gr.Blocks() as demo:
     gr.Markdown("""
     # Cleric Call Logs Summarize Agent
     
-    Please place the URLs in the Call Logs URLs text box, separated by new line.
-    Place your question to this call logs, then submit! 
+    ### Instructions:
+    1. Enter the URLs in the "Call Log URLs" text box, separating each URL with a new line.
+    2. Add your question related to these call logs.
+    3. Click the "Submit" button to proceed.
     """)
     error_box = gr.Textbox(label="Error", visible=False)
     with gr.Row(equal_height=True):
-        call_logs_box = gr.Textbox(label='Call Logs URLs', scale=2)
-        facts_box = gr.Textbox(label='Extracted Facts', scale=2)
+        call_logs_box = gr.Textbox(label='Call Log URLs', lines=10)
+        facts_box = gr.Textbox(label='Extracted Facts', lines=10)
 
     question_box = gr.Textbox(label='Question')
     submit_btn = gr.Button("Submit")
 
     submit_btn.click(
         fetch_facts,
-        inputs=[call_logs_box, question_box],
+        inputs=[question_box, call_logs_box],
         outputs=facts_box
     )
 
-# iface = gr.Interface(
-#     fn=fetch_facts,
-#     inputs=["text", "text"],
-#     outputs="text",
-#     allow_flagging="never",
-#     title="Cleric Call Logs Summarize Agent"
-# )
 demo.launch()
 
